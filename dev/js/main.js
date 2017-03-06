921a8cce31d0834ca447f0741c6ca2960f0f1764
IgorRomG3 <webroma57@gmail.com>
2017-03-06 02:32:01 +0200

    added js code for header-down-btn , begining main

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,11 +1,26 @@
-var mainNav = document.getElementById('navbar');
-
-var mainNavSourceBottom = mainNav.getBoundingClientRect().bottom + window.pageYOffset;
+// FOR FIXED NAVBAR
+var mainNav = document.getElementById('navbar'),
+    lightLogo = document.getElementsByClassName('logo-light')[0],
+    darkLogo = document.getElementsByClassName('logo-dark')[0],
+    mainNavSourceBottom = mainNav.getBoundingClientRect().bottom + window.pageYOffset;
 
     window.onscroll = function() {
       if (window.pageYOffset > mainNavSourceBottom) {
         mainNav.classList.add('fixed');
+        lightLogo.style.display = "none";
+        darkLogo.style.display = "block";
       } else if (window.pageYOffset < mainNavSourceBottom) {
         mainNav.classList.remove('fixed');
+        lightLogo.style.display = "block";
+        darkLogo.style.display = "none";
       }
     };
+
+// FOR HEADER DOWN BUTTON
+var downBtn = document.getElementsByClassName('header-down-btn')[0],
+    downBtnLocation = downBtn.getBoundingClientRect().bottom + window.pageYOffset;
+
+downBtn.onclick = function() {
+  window.scrollTo(downBtnLocation,670);
+} 
+  
