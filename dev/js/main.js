19263aa8879cf9e3180fe5c3a13eadb9d02c4e71
IgorRomG3 <webroma57@gmail.com>
2017-03-13 22:42:15 +0200

    small fixes + working with main.js

--- dev/js/main.js
+++ dev/js/main.js
@@ -33,7 +33,9 @@ downBtn.onclick = function () {
     }
   }, 10);
 }
+
 //NUMBERS
+
 var numContainer = document.getElementsByClassName('donelist')[0];
 var num = document.getElementsByClassName('numbers');
 var countNum = document.getElementsByClassName('counting');
@@ -45,71 +47,76 @@ var offset4 = 0;
 window.addEventListener("scroll", getCountingClass);
 
 function getCountingClass() {
-  if (window.pageYOffset >= numContainerLocation) {
+  if (window.pageYOffset > 900) {
     for (var i = 0; i < num.length; i++) {
       num[i].classList.add('counting');
     }
   }
 }
-if (num[0].classList.contains('counting') == true && num[1].classList.contains('counting') == true && num[2].classList.contains('counting') == true && num[3].classList.contains('counting') == true) {
-  var numOne = setInterval(function () {
-    var target = 548;
-    offset1 += 1;
-    if (offset1 >= target) {
-      clearInterval(numOne);
-    }
-    num[0].innerHTML = offset1;
-  }, 10);
-  var numTwo = setInterval(function () {
-    var target = 1465;
-    offset2 += 1;
-    if (offset2 >= target) {
-      clearInterval(numTwo);
-    }
-    num[1].innerHTML = offset2;
-  }, 1);
-  var numThree = setInterval(function () {
-    var target = 612;
-    offset3 += 1;
-    if (offset3 >= target) {
-      clearInterval(numThree);
-    }
-    num[2].innerHTML = offset3;
-  }, 10);
-  var numFour = setInterval(function () {
-    var target = 735;
-    offset4 += 1;
-    if (offset4 >= target) {
-      clearInterval(numFour);
-    }
-    num[3].innerHTML = offset4;
-  }, 10);
+if (num[0].classList.contains('counting')) {
+  // var numOne = setInterval(function () {
+  //   var target = 548;
+  //   offset1 += 1;
+  //   if (offset1 >= target) {
+  //     clearInterval(numOne);
+  //   }
+  //   countNum[0].innerHTML = offset1;
+  // }, 12);
+  // var numTwo = setInterval(function () {
+  //   var target = 1465;
+  //   offset2 += 1;
+  //   if (offset2 >= target) {
+  //     clearInterval(numTwo);
+  //   }
+  //   countNum[1].innerHTML = offset2;
+  // }, 1);
+  // var numThree = setInterval(function () {
+  //   var target = 612;
+  //   offset3 += 1;
+  //   if (offset3 >= target) {
+  //     clearInterval(numThree);
+  //   }
+  //   countNum[2].innerHTML = offset3;
+  // }, 10);
+  // var numFour = setInterval(function () {
+  //   var target = 735;
+  //   offset4 += 1;
+  //   if (offset4 >= target) {
+  //     clearInterval(numFour);
+  //   }
+  //   countNum[3].innerHTML = offset4;
+  // }, 9);
+  alert('hello');
 }
-//CARUSEL
+
+//BLAQUOTE-CARUSEL
 
 var caruselBlaq = document.getElementsByClassName('blaquote-wrap')[0],
-  ulBlaq = caruselBlaq.querySelector('ul');
-var dotsContainerBlaq = document.getElementsByClassName('dots')[0];
-var dotsBlaq = document.querySelectorAll('.dots > span');
-var widthBlaq = -1160;
-var selectedDot;
-var position = 0;
-dotsContainerBlaq.addEventListener('click',function(e) {
+  ulBlaq = caruselBlaq.querySelector('ul'),
+  dotsContainerBlaq = document.getElementsByClassName('carusel-blaquote-dots')[0],
+  dotsBlaq = document.querySelectorAll('.carusel-blaquote-dots > li'),
+  widthBlaq = -1160,
+  selectedDot,
+  position = 0;
+
+dotsContainerBlaq.addEventListener('click', function (e) {
   e = e || event;
   var target = e.target;
-  getSliderPosition(dotsBlaq,target,ulBlaq,widthBlaq);
-  setActiveDot(target,['active'],dotsBlaq);
+  setSliderPosition(dotsBlaq, target, ulBlaq, widthBlaq);
+  setActiveDot(target, ['active'], dotsBlaq);
 });
-function getSliderPosition(container,obj,slider,step) {
-  for(var i = 0; i < container.length; i++) {
-    if(container[i] == obj) {
-      position = i*step;
+
+function setSliderPosition(container, obj, slider, step) {
+  for (var i = 0; i < container.length; i++) {
+    if (container[i] == obj) {
+      position = i * step;
     }
   }
   slider.style.marginLeft = position + 'px';
 }
-function setActiveDot(node,addClass,dotsWrap) {
-  if(selectedDot) {
+
+function setActiveDot(node, addClass, dotsWrap) {
+  if (selectedDot) {
     selectedDot.classList.remove(addClass);
   }
   dotsWrap[0].classList.remove(addClass);
@@ -120,14 +127,14 @@ function setActiveDot(node,addClass,dotsWrap) {
 //BLOG-CARUSEL
 
 var caruselBlog = document.getElementsByClassName('blog-wrap')[0],
-    ulBlog = caruselBlog.querySelector('ul'),
-    dotsContainerBlog = document.getElementsByClassName('carusel-blog-dots')[0],
-    dotsBlog = document.querySelectorAll('.carusel-blog-dots > li'),
-    widthBlog = -400;
+  ulBlog = caruselBlog.querySelector('ul'),
+  dotsContainerBlog = document.getElementsByClassName('carusel-blog-dots')[0],
+  dotsBlog = document.querySelectorAll('.carusel-blog-dots > li'),
+  widthBlog = -400;
 
-dotsContainerBlog.addEventListener('click',function(e) {
+dotsContainerBlog.addEventListener('click', function (e) {
   e = e || event;
   var target = e.target;
-  getSliderPosition(dotsBlog,target,ulBlog,widthBlog);  /*102*/
-  setActiveDot(target,['selected'],dotsBlog);           /*111*/
+  setSliderPosition(dotsBlog, target, ulBlog, widthBlog); /*108*/
+  setActiveDot(target, ['selected'], dotsBlog); /*117*/
 });
